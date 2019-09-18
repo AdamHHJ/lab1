47f2b0a70c4e524f4de696f42ec1fc952012c3e5
AdamHHJ <1114537288@qq.com>
2019-09-18 17:15:28 -0400

    Set button click behavior

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -1,14 +1,30 @@
 package edu.temple.helloworld;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewToChange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewToChange = findViewById(R.id.textViewToChange);
+
+        findViewById(R.id.clickButton)
+                .setOnClickListener(new View.OnClickListener() {
+
+                    //set onClick behavior
+                    
+                    @Override
+                    public void onClick(View v) {
+                        textViewToChange.setText("You have clicked the button! ");
+                    }
+                });
     }
 }
